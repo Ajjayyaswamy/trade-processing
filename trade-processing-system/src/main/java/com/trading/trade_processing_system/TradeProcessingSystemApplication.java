cba09c260ae18d8e8e2aeb3ba4e8afa10c92ef3b
AJJAYYASWAMY S M <144276625+Ajjayyaswamy@users.noreply.github.com>
2025-06-19 21:48:48 +0530

    initial

---
+++ trade-processing-system/src/main/java/com/trading/trade_processing_system/TradeProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.trading.trade_processing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeProcessingSystemApplication.class, args);
+	}
+
+}
---
+++ trade-processing-system/src/test/java/com/trading/trade_processing_system/TradeProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.trade_processing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
